b06314eb298b22c6b6fa6b371dbad57cea0de3eb
Patrick Kim <pkim050@gmail.com>
2019-03-21 22:29:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,12 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    # row has id name grade in an array.
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
