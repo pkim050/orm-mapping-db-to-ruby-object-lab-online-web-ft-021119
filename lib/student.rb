87a7bf0e47834a3e4b2f20b2db861c2009744f5f
Patrick Kim <pkim050@gmail.com>
2019-03-21 22:50:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,4 +69,11 @@ class Student
       SQL
     DB[:conn].execute(sql).map {|element| self.new_from_db(element)}
   end
+
+  def self.first_x_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+      SQL
+    DB[:conn].execute(sql, number).map {|element| self.new_from_db(element)}
+  end
 end
